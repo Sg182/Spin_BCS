d9388c4907249aaebf55f4c924d7b41d96437448
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-02-19 21:27:56 -0600

    created .txt to store opt eta s

--- J1J2_energy.py
+++ J1J2_energy.py
@@ -2,20 +2,26 @@ from Overlap import bcs_overlap, Sz,overlap_J1J2
 import numpy as np
 from scipy.optimize import minimize
 from parameter import Nx,Ny,J
- 
+import os
+
+
+eta_file = "eta_optimized.txt"
+
+if os.path.exists(eta_file): # checks if the file exists or not
+    eta_initial = np.loadtxt(eta_file) # load eta_optimized from the file
+
+else:   # otherwise do an intial guess
+    eta_initial = np.random.uniform(-1,1,Nx*Ny) 
 
 def energy_bcs_J1J2(eta,J):
     return (overlap_J1J2(J,eta,eta)/(bcs_overlap(eta,eta)))
 
-# initial guess for eta
 
-eta_initial = np.random.uniform(-1,1,Nx*Ny)   # intializes random eta
- 
 result = minimize(energy_bcs_J1J2, eta_initial, args=(J,), method='BFGS')
 
 eta_optimized = result.x
 final_energy = result.fun
-print(f"sBCS J1J2 Energy: {final_energy}")
+print(f"sBCS J1J2 Energy:{J}      {final_energy}")
 #print(eta_optimized)
 
 Sz_sum = 0
@@ -24,8 +30,9 @@ for i in range(1,Nx*Ny+1):  # This calculates Sz for each site
     #print(Sz(eta_optimized,eta_optimized,i)/bcs_overlap(eta_optimized,eta_optimized))
     Sz_sum += Sz(eta_optimized,eta_optimized,i)/bcs_overlap(eta_optimized,eta_optimized)
 
-print("The global Sz is:",Sz_sum)
+#print("The global Sz is:",Sz_sum)
 
+np.savetxt(eta_file,eta_optimized)
 
 with open("energy_J1J2.txt","a") as file: # open a file and write the results to it
-    file.write(f"{J} {final_energy}\n")
+    file.write(f"{J}    {final_energy}\n")
--- XXZ_energy.py
+++ XXZ_energy.py
@@ -1,21 +1,31 @@
 from Overlap import *
 import numpy as np
 from scipy.optimize import minimize
-from parameter import Delta
- 
+import os
+#from parameter import Delta
+#import sys
+Delta = -2.0 #float(sys.argv[1])  # Read Delta from command-line argument
+
 
 def energy_bcs_XXZ(eta,Delta):
     return (overlap_XXZ(Delta,eta,eta))/(bcs_overlap(eta,eta))
 
-# initial guess for eta
+eta_file = "eta_optimized.txt"
 
-eta_initial = np.random.uniform(-1,1,Nx*Ny)   # intializes random eta
- 
+
+if os.path.exists(eta_file): # checks if the file exists or not
+    eta_initial = np.loadtxt(eta_file) # load eta_optimized from the file
+
+else:   # otherwise do an intial guess
+    eta_initial = np.random.uniform(-1,1,Nx*Ny) 
+
+
+# initialize minimization 
 result = minimize(energy_bcs_XXZ, eta_initial, args=(Delta,), method='BFGS')
 
 eta_optimized = result.x
 final_energy = result.fun
-print(f"sBCS XXZ Energy: {final_energy}")
+print(f"sBCS XXZ Energy:{Delta}        {final_energy}")
 #print(eta_optimized)
 
 Sz_sum = 0
@@ -24,12 +34,12 @@ for i in range(1,Nx*Ny+1):  # This calculates Sz for each site
     #print(Sz(eta_optimized,eta_optimized,i)/bcs_overlap(eta_optimized,eta_optimized))
     Sz_sum += Sz(eta_optimized,eta_optimized,i)/bcs_overlap(eta_optimized,eta_optimized)
 
-print("The global Sz is:",Sz_sum)
+#print("The global Sz is:",Sz_sum)
 
+np.savetxt(eta_file,eta_optimized) # save the eta_optimized in the .txt
 
 with open("energy_XXZ.txt","a") as file:  # saves the result to .txt file
-    file.write(f"{Delta} {final_energy}\n")
+    file.write(f"{Delta}    {final_energy}\n")
  
-
 
 
