434d3bd14a3e4604b0fb6d13c7a6a60324dd1330
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-04-06 20:02:13 -0500

    changed the functions into class

--- overlap_uv.py
+++ overlap_uv.py
@@ -2,7 +2,9 @@ from neighbor import *
 import numpy as np
 # Only for Real u,v
 
-def bcs_overlap(theta,Nsites):   # computes overlap between two BCS states
+
+
+'''def bcs_overlap(theta,Nsites):   # computes overlap between two BCS states
     overlap = 1
     
     for k in range(Nsites):
@@ -60,23 +62,107 @@ def XXZ_1D_overlap(theta,Nsites,Delta):  # function to calculate XXZ_Energy in 1
             sum_energy += sum_splus_sminus + Delta*sum_SzSz
 
     return sum_energy
+
+def J1J2_2D_overlap(theta,Nsites,J):  # function to calculate XXZ_Energy in 1D
+    total_overlap_J1 = 0
+    total_overlap_J2 = 0
+    for i in range(1,Nsites+1):
+        x,y = inverse_mapping(i,Ny)  # tuple unpacking
+
+        neighbour_sites = neighbor_square(x,y,Nx,Ny)  # returns neighboring sites for i
+        for j in neighbour_sites:
+            if i>= j:
+                continue
+
+            sum_splus_sminus = Splus_Sminus(theta,i-1,j-1)
+            sum_SzSz = S_zS_z(theta,i-1,j-1)
+            total_overlap_J1 += sum_splus_sminus + sum_SzSz
+
     
+    for i in range(1,Nsites+1):
+        x,y = inverse_mapping(i,Ny)  # tuple unpacking
 
-'''cos_part_p = (np.cos(np.pi/2))
-sin_part_p = (np.sin(np.pi/2))
-cos_part_q =  (np.cos(np.pi/2))
-sin_part_q = (np.sin(np.pi/2))
-print(cos_part_p)
+        neighbour_sites = second_neighbor(x,y,Nx,Ny)  # returns neighboring sites for i
+        for j in neighbour_sites:
+            if i>= j:
+                continue
 
-prefactor_splus_sminus = cos_part_p*sin_part_p*cos_part_q*sin_part_q
-print(prefactor_splus_sminus)
+            sum_splus_sminus = Splus_Sminus(theta,i-1,j-1)
+            sum_SzSz = S_zS_z(theta,i-1,j-1)
+            total_overlap_J2 += J*(sum_splus_sminus + sum_SzSz)
+    sum_energy = total_overlap_J1 + total_overlap_J2
 
-cos_part_p = (np.cos(np.pi/4))**2
-sin_part_p = (np.sin(np.pi/4))**2
-cos_part_q =  (np.cos(np.pi/4))**2
-sin_part_q = (np.sin(np.pi/4))**2  
-numerator = ((-cos_part_p + sin_part_p)*(-cos_part_q + sin_part_q))
-denominator = ((cos_part_p + sin_part_p)*(cos_part_q+sin_part_q))
-prefactor = (1/4)*((numerator)/denominator)
-print(denominator)'''
-    
+    return sum_energy   ''' 
+
+ 
+
+class BCSHamiltonian:
+    def __init__(self, theta, Nx, Ny):
+        self.theta = theta
+        self.Nx = Nx
+        self.Ny = Ny
+        self.Nsites = Nx * Ny
+
+    def bcs_overlap(self):
+        overlap = 1
+        for k in range(self.Nsites):
+            overlap *= (np.cos(self.theta[k])**2 + np.sin(self.theta[k])**2)
+        return overlap
+
+    def Sz(self, i):
+     
+        cos_sq = np.cos(self.theta[i])**2
+        sin_sq = np.sin(self.theta[i])**2
+        numerator = -cos_sq + sin_sq
+        denominator = cos_sq + sin_sq
+        prefactor_Sz = 0.5 * numerator / denominator
+        return self.bcs_overlap() * prefactor_Sz
+
+    def Splus_Sminus(self, p, q):
+        return np.cos(self.theta[p]) * np.sin(self.theta[p]) * \
+               np.cos(self.theta[q]) * np.sin(self.theta[q])
+
+    def S_zS_z(self, p, q):
+        cp, sp = np.cos(self.theta[p])**2, np.sin(self.theta[p])**2
+        cq, sq = np.cos(self.theta[q])**2, np.sin(self.theta[q])**2
+        numerator = (-cp + sp) * (-cq + sq)
+        denominator = (cp + sp) * (cq + sq)
+        return 0.25 * numerator / denominator
+
+    def XXZ_overlap(self, Delta):
+        sum_energy = 0
+        for i in range(1, self.Nsites + 1):
+            x, y = inverse_mapping(i, self.Ny)
+            neighbors = neighbor_square(x, y, self.Nx, self.Ny)
+            for j in neighbors:
+                if i >= j:
+                    continue
+                p, q = i - 1, j - 1
+                sum_energy += self.Splus_Sminus(p, q) + Delta * self.S_zS_z(p, q)
+        return sum_energy
+
+    def J1J2_2D_overlap(self, J):
+        total_overlap_J1 = 0
+        total_overlap_J2 = 0
+
+        # First nearest neighbors
+        for i in range(1, self.Nsites + 1):
+            x, y = inverse_mapping(i, self.Ny)
+            neighbors = neighbor_square(x, y, self.Nx, self.Ny)
+            for j in neighbors:
+                if i >= j:
+                    continue
+                p, q = i - 1, j - 1
+                total_overlap_J1 += self.Splus_Sminus(p, q) + self.S_zS_z(p, q)
+
+        # Second nearest neighbors
+        for i in range(1, self.Nsites + 1):
+            x, y = inverse_mapping(i, self.Ny)
+            neighbors = second_neighbor(x, y, self.Nx, self.Ny)
+            for j in neighbors:
+                if i >= j:
+                    continue
+                p, q = i - 1, j - 1
+                total_overlap_J2 += J * (self.Splus_Sminus(p, q) + self.S_zS_z(p, q))
+
+        return total_overlap_J1 + total_overlap_J2
